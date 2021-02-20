b5675c11788fe20251350530fa4072f33328c26a
ChienChang <ryan_zhangjian@foxmail.com>
2021-02-20 22:07:30 +0800

    添加方法：针对评测得分，转化为对应RGB值（#hex），越高越红。

--- src/main/java/com/edu/cup/ToolBag.java
+++ src/main/java/com/edu/cup/ToolBag.java
@@ -14,26 +14,29 @@ import java.util.Set;
 
 public class ToolBag {
 
-//    public static String MaptoString(Map map) {
-//        Set<String> keySet = map.keySet();
-//        String mapString = "{";
-//        for (String item : keySet) {
-//            String valueMap = (String) map.get(item);
-//            valueMap =valueMap .replace("，",",");
-//            if (item.equals("cites_per_year")){
-//                valueMap = valueMap .replace(" - ","\\\",");
-//                valueMap = valueMap .replace(": ",":\\\"");
-//                valueMap = valueMap .replace("\'","\\\"");
-//                valueMap = valueMap .replace("}","\\\"}");
-//                mapString = mapString + "\\\"" + item + "\\\"" + ":" + valueMap + ",";
-//            }else{
-//                mapString = mapString + "\\\"" + item + "\\\"" + ":" + "\\\"" + valueMap + "\\\"" + ",";
-//            }
-//        }
-//        mapString = mapString.substring(0,mapString.length()-1);
-//        mapString = mapString+"}";
-//        return mapString;
-//    }
+/*****一开始给python传消息使用的处理
+    public static String MaptoString(Map map) {
+        Set<String> keySet = map.keySet();
+        String mapString = "{";
+        for (String item : keySet) {
+            String valueMap = (String) map.get(item);
+            valueMap =valueMap .replace("，",",");
+            if (item.equals("cites_per_year")){
+                valueMap = valueMap .replace(" - ","\\\",");
+                valueMap = valueMap .replace(": ",":\\\"");
+                valueMap = valueMap .replace("\'","\\\"");
+                valueMap = valueMap .replace("}","\\\"}");
+                mapString = mapString + "\\\"" + item + "\\\"" + ":" + valueMap + ",";
+            }else{
+                mapString = mapString + "\\\"" + item + "\\\"" + ":" + "\\\"" + valueMap + "\\\"" + ",";
+            }
+        }
+        mapString = mapString.substring(0,mapString.length()-1);
+        mapString = mapString+"}";
+        return mapString;
+    }
+ */
+
     public static String MaptoString(Map map) {
         Set<String> keySet = map.keySet();
         String mapString = "{";
@@ -63,5 +66,24 @@ public class ToolBag {
         maplistSting = maplistSting.substring(0,maplistSting.length()-1);
         maplistSting = maplistSting + "]";
         return maplistSting;
+    }
+
+    public static String RgbReturn(String HIndex){
+        int Rcolor = 255;
+        int GBcolar = 126;
+        int hIndex = (Integer.parseInt(HIndex));
+
+        if (hIndex > 254){
+            return "#" + Integer.toHexString(255) + Integer.toHexString(0) + Integer.toHexString(0) + Integer.toHexString(0) + Integer.toHexString(0);
+        }
+        else {
+            GBcolar = GBcolar -hIndex;
+            if(GBcolar > 15){
+                return "#"+Integer.toHexString(Rcolor)+Integer.toHexString(GBcolar)+Integer.toHexString(GBcolar);
+            }
+            else {
+                return "#"+Integer.toHexString(Rcolor) + Integer.toHexString(0) + Integer.toHexString(GBcolar) + Integer.toHexString(0) + Integer.toHexString(GBcolar);
+            }
+        }
     }
 }
